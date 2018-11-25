b00778e0581e0bd51d4b5b76da92ff2f3ad9af80
ken <kenbusiness30@gmail.com>
2018-11-25 20:33:52 +0900

    Add filter, DBUtil, Kadai.models

--- src/models/Kadailist.java
+++ src/models/Kadailist.java
@@ -1,5 +1,32 @@
 package models;
 
-public class Kadailist {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
+@Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "AllTasklists",
+            query = "select t from Tasklist as t"
+            )
+})
+@Table(name = "tasks")
+public class Kadailist {
+    @Id
+    @Column(name = "content", nullable=false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String content;
+
+    public String getContent(){
+        return this.content;
+    }
+    public void setContent(String content){
+        this.content = content;
+    }
 }
